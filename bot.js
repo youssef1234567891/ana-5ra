6522c3b58c0255eb5272a954d1ebe1b93c7f23af
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-07-16 16:28:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,25 @@
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'اسم الشات العام حقك');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome to the server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                      
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+                                       
+     .setFooter("**SERVER NAME **")
+        .setTimestamp()
+    
+      channel.sendEmbed(embed);
+    });
+    
 var prefix = ".";
 
 client.on("message", message => {
