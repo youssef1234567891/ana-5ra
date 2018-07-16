72e788bca8c16dcbc45cb8b264aed7d97e9aef76
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-07-16 16:37:40 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,63 +1,25 @@
-client.on('guildMemberAdd', member => {
-    let channel = member.guild.channels.find('name', 'ĉђฮτ💭آلَشّـآتُ');
-    let memberavatar = member.user.avatarURL
-      if (!channel) return; 
-    let embed = new Discord.RichEmbed()
-        .setColor('RANDOM')
-        .setThumbnail(memberavatar)
-        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
-        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome to the server, ${member}`)
-        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
-                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
-               
-                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
-                      
-                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
-                                       
-     .setFooter("**SERVER NAME **")
-        .setTimestamp()
-    
-      channel.sendEmbed(embed);
+client.on('ready', () => {
+    console.log('----------------');
+  console.log('By iMrKBOOSH');
+console.log('----------------');
+console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(` ON ${client.guilds.size}  Servers `,"https://www.twitch.tv/")
+client.user.setStatus(".")
+});
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+client.on('guildMemberAdd', member=> {
+
+    client.channels.get("466972243176587274").send(`***__ الكلام الي ترحيب  ${member}___***`);
     });
-    
-var prefix = ".";
-
-client.on("message", message => {
-
-            if (message.content.startsWith(prefix + "obc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
-});
-    console.log(`Logged in as ${client.user.tag}!`);
-    client.user.setGame(`هيتهات النوب`,"http://twitch.tv/y04zgamer")
-    client.user.setStatus("dnd")
- });
-Client.on("message", message => {
-    var prefix = ".";
- 
-            var args = message.content.substring(prefix.length).split(" ");
-            if (message.content.startsWith(prefix - "bc")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
- if (!args[1]) {
-                                let embed3 = new Discord.RichEmbed()
-                                .setDescription(":white_check_mark: | تم ارسال للكل رساله فارغه")
-                                .setColor("#FF0000")
-                                message.channel.sendEmbed(embed3);
-                            } else {
-
-                            let embed4 = new Discord.RichEmbed()
-                                                            .setDescription(':white_check_mark: | تم ارسال للكل الرساله !')
-                                                            .setColor("#008000")
-                                message.channel.sendEmbed(embed4);
-                                                      message.delete();
-                            }
-                          }
-});
-client.login("NDY4MzQwOTc4ODQ0MDQxMjE3.Di3wGg.jmdIAx2znpWVnooewYZhxp6nrtI" );
+client.login('NDY4MzQwOTc4ODQ0MDQxMjE3.Di4_HQ.wxrpkbDetZDxWp1Y97w4BxOt4Aw'); // هنا ضع التوكن المتواجد بالشرح الذي انشئته
