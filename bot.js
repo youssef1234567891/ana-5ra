58c5a3e5aebbfa5acbbb3816f20a78a4c49d78b1
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-07-21 20:35:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-onst Discord = require("discord.js");
+const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
