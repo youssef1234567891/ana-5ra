e266edfbca93d71a1dcc9526d340f872582a0fda
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-07-23 18:42:18 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -167,5 +167,16 @@ client.on('message', message => {
     }
     });
 ;
+client.on('guildMemberAdd', member => {
+    var embed = new Discord.RichEmbed()
+    .setThumbnail(member.user.avatarURL)
+  .addField("***شكرا الانضمامك الينا***" ,member.user.username )
+    .setDescription('***بكل حب واحترام وشوق نستقبلك ونتمنى لك قضآء أجمل اللحظات ولآوقات معنا***')
+    .setColor('RANDOM')
+    .setImage('https://cdn.discordapp.com/attachments/451513900655968256/470992644038721537/IMG-20180624-WA0012.jpg')
+var channel =member.guild.channels.find('name', 'chat-arabjoker')
+if (!channel) return;
+channel.send({embed : embed});
+});
 client.login("NDcwOTYzNDc3MDEyMjgzMzk0.Djd6_w.Wxmk5TBrNgGDS-UMhEHStMIakBY");
 //CODES ReBeL
