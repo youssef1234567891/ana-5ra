38c90db9bacaea76282b250d88f5015dee372fd3
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-08-19 03:23:39 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -252,5 +252,6 @@ client.on("message", message => {
       message.guild.member(user).addRole(muteRole).then(() => {
   return message.reply("**:white_check_mark: .. تم اعطاء العضو ميوت كتابي**").catch(console.error);
   });
-client.login("NDgwNTA3NjI2MzMzOTI5NDg0.Dlo33Q.MF2l0PMnvBePxSecbggTsTz9d2M");
+
+client.login("NDgwNTA3NjI2MzMzOTI5NDg0.DlpX7g.PKp0MLWTbzjAME4tfM8PDttTRVs");
 //CODES ReBeL
