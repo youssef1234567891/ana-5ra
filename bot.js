1615310973ed7ba9717078b0785f2918cbd1633c
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-08-19 02:26:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -209,7 +209,6 @@ client.on("message", message => {
             return;
         }
     });
-
  client.on("message", message => {
       if (message.content === "%help") {
        const embed = new Discord.RichEmbed() 
@@ -239,7 +238,6 @@ client.on("message", message => {
      
      }
      });
-
      client.on("message", message => {
       if (message.content === "=help") {
        const embed = new Discord.RichEmbed() 
@@ -287,48 +285,6 @@ let embed = new Discord.RichEmbed()
 .addField('**WebSocket:**',api + " ms :signal_strength: ")
 message.channel.send({embed:embed});
 }
-});
-client.on('message', message => {
-    if (message.content.startsWith("=stats")) {
-    message.channel.send({
-        embed: new Discord.RichEmbed()
-            .setColor('RANDOM')
-            .setTitle('Stats Bot / Info ')
-            .addField('``Uptime``', timeCon(process.uptime()), true)
-            .addField('``Ping Is``' , `${Date.now() - message.createdTimestamp}` + '``Ms``', true)
-            .addField('``RAM Usage``', `${(process.memoryUsage().rss / 1048576).toFixed()}MB`, true)
-            .addField('``Guild Count``', client.guilds.size, true)
-            .addField('``Bot In channel``' , `${client.channels.size}` , true)
-            .addField('``Users rout``' ,`${client.users.size}` , true)
-            .addField('``Name Bot Or tag``' , `${client.user.tag}` , true)
-            .addField('``Bot Id``' , `${client.user.id}` , true)
-            .setFooter('SmiLeBoT / Team')
-    })
-}
-});
-
-
-function timeCon(time) {
-    let days = Math.floor(time % 31536000 / 86400)
-    let hours = Math.floor(time % 31536000 % 86400 / 3600)
-    let minutes = Math.floor(time % 31536000 % 86400 % 3600 / 60)
-    let seconds = Math.round(time % 31536000 % 86400 % 3600 % 60)
-    days = days > 9 ? days : '0' + days
-    hours = hours > 9 ? hours : '0' + hours
-    minutes = minutes > 9 ? minutes : '0' + minutes
-    seconds = seconds > 9 ? seconds : '0' + seconds
-    return `${days > 0 ? `${days}:` : ''}${(hours || days) > 0 ? `${hours}:` : ''}${minutes}:${seconds}`
-});
-client.on("message", (message) => {
-    if (message.content.startsWith("=ban")) {
-      if(!message.member.hasPermission('BAN_MEMBERS')) return message.reply(':warning: ماعندك الصلاحيات');
-        var member= message.mentions.members.first();
-        member.ban().then((member) => {
-            message.channel.send(member.displayName + " مع السلامه :wave: ");
-        }).catch(() => {
-            message.channel.send("Error -_-");
-        });
-    }
 });
 client.on("message", message => {
     if (message.author.bot) return;
