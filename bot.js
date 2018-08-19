eb6074402cb8fd4fb1228199a47cc4c41a21692f
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-08-19 02:28:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -209,6 +209,19 @@ client.on("message", message => {
             return;
         }
     });
+client.on('message', message => {
+    if (message.content === "=help") {
+    let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setFooter(`© SHYBOY_05 ™.`, 'https://images-ext-2.discordapp.net/external/X9SanEG0s7Dtv3krTgy-kod_fj6JRVJ2AG4JryCiiz0/%3Fsize%3D2048/https/cdn.discordapp.com/avatars/375761288518828042/fffa31c797e88cf059dd6db424ff456a.png?width=80&height=80')
+  .setColor("#000000")
+  .addField("Done | تــــم" , ":envelope: | :sleuth_or_spy::skin-tone-3:شيك علي في الخاص")
+  
+  
+  
+  message.channel.sendEmbed(embed);
+  }
+  });
  client.on("message", message => {
       if (message.content === "%help") {
        const embed = new Discord.RichEmbed() 
