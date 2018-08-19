65bf30f9a4ead72283ce6ac8d14bc112dd11637b
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-08-19 04:45:09 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -293,24 +293,4 @@ client.on('ready', function(){//npm i ms
     }, ms);
 
 });
-client.on("message", message => {
-    var args = message.content.substring(prefix.length).split(" ");
-    if (message.content.startsWith(prefix + "clear")) {
-        if(!message.channel.guild) return message.reply('**:x: اسف لكن هذا الامر للسيرفرات فقط **');         
-if(!message.member.hasPermission('MANAGE_MESSAGES')) return message.reply('**⚠  لا يوجد لديك صلاحية لمسح الشات**');
-var msg;
-msg = parseInt();
-
-message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
-message.channel.sendMessage("", {embed: {
-title: "``تــم مسح الشات ``",
-color: 0x06DF00,
-footer: {
-  
-}
-}}).then(msg => {msg.delete(3000)});
-                  }
-
-
-});
 client.login("NDgwNTA3NjI2MzMzOTI5NDg0.DlpnlA.lFgxmsrR3dEn-xlC5xdDdFu1vvY")
