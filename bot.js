d6c9f00806e0356199e7962a4dd295231a000f51
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-08-19 02:40:50 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -250,19 +250,6 @@ client.on('message', message => {
      }
      });
 client.on('message', message => {
-    if(message.content == '=member') {
-    const embed = new Discord.RichEmbed()
-    .setDescription(`**Members infoðŸ”‹
-:green_heart: online:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
-:heart:dnd:       ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
-:yellow_heart: idle:      ${message.guild.members.filter(m=>m.presence.status == 'idle').size}   
-:black_heart: offline:   ${message.guild.members.filter(m=>m.presence.status == 'offline').size} 
-:blue_heart:   all:  ${message.guild.memberCount}**`)         
-         message.channel.send({embed});
-
-    }
-  });
-client.on('message', message => {
     if(!message.channel.guild) return;
 if (message.content.startsWith('=ping')) {
 if(!message.channel.guild) return;
