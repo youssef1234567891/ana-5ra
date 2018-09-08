5da21d451c5d0c3ef29ad4616e44af8d170ff130
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-09-08 14:29:21 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,5 +10,8 @@ client.on('message', msg => {
     msg.reply('Pong!');
   }
 });
-
+client.on('ready',async () => {
+let channel = hero.channels.find(r => r.id === 'اي دي الروم' && r.type === 'voice');
+channel.join().catch(e => console.error(e));
+});
 client.login('NDgyMzQxNTk0MTYzMzE0Njg4.DnSwSw.iGeE2waZTY3v0ptNbKSndL1OZks');
