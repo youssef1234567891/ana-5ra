fd1f0bb74d84e79ac418cc20c624b11203c6c671
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-09-08 13:53:04 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,14 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+client.login('NDgyMzQxNTk0MTYzMzE0Njg4.DnSwSw.iGeE2waZTY3v0ptNbKSndL1OZks');
