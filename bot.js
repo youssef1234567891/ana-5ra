f6f038520076482c3722f7aad24f5d5edc60a10f
itzm7md <41285872+itzm7md@users.noreply.github.com>
2018-09-10 00:11:19 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,59 @@
-const Discord = require("discord.js");
+var ServerID = "464493334849912848"; //اي دي السيرفر
+var ChannelID = "464493334849912856";// اي دي الروم
+
+/*	
+ 
+Codes server - !.Zeyad' , ♚Roýale#5555
+                                                                                                            
+                                                                                                            
+  ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______   ______ 
+ /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/  /_____/ 
+                                                                                                            
+                                                                                                            
+		
+__________              __________        __   
+\______   \_______  ____\______   \ _____/  |_ 
+ |     ___/\_  __ \/  _ \|    |  _//  _ \   __\
+ |    |     |  | \(  <_> )    |   (  <_> )  |  
+ |____|     |__|   \____/|______  /\____/|__|  
+                                \/             
+_________                    .___.__  __          
+\_   ___ \_______   ____   __| _/|__|/  |_  ______
+/    \  \/\_  __ \_/ __ \ / __ | |  \   __\/  ___/
+\     \____|  | \/\  ___// /_/ | |  ||  |  \___ \ 
+ \______  /|__|    \___  >____ | |__||__| /____  >
+        \/             \/     \/               \/ 
+Codes server - !.Zeyad' , ♚Roýale#5555
+   _____  .__                     
+  /     \ |__| ____   ___________ 
+ /  \ /  \|  |/    \_/ __ \_  __ \
+/    Y    \  |   |  \  ___/|  | \/
+\____|__  /__|___|  /\___  >__|   
+        \/        \/     \/       
+*/
+const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('message', message => {
-if (message.content === "test") {
-  var ms = 120000;
-      var interval = setInterval (function () {
-        message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-         message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-         message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-         message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-         message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-         message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-         message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-         message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-         message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-         message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-         message.channel.send("لا إله الا الله, سبحان الله, الحمدالله, الله اكبر")
-      }, ms); 
-    }
-});
+client.on('warn', console.warn);
 
+client.on('error', console.error);
+// Codes server - !.Zeyad' , ♚Roýale#5555
+
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Zeyad,Royale','\n','ProBot credits miner is ready!'));// Codes Server` - iiZeyad , Roýale"1.5k 🍷#1111
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));// Codes server - !.Zeyad' , ♚Roýale#5555
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send(Math.random().toString(36).substring(7)) // Codes server - !.Zeyad' , ♚Roýale#5555
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
 client.login('NDg4MDI3NzkzNTQ0NzA4MDk3.DnW-tg.NL-xJk8Ys3t2SRO5g9OanTvXKps');
